c44318b6f8650ec27d6383ff9dd1ed482edc4949
Russell Fraze <russell.fraze@gmail.com>
2022-07-14 15:02:29 -0700

    speed up and finish

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const score = document.querySelector('#score')
 let result = 0
 let hitPosition 
 let currentTime = 60
+let timerId = null
 
 function randomSquare() {
     squares.forEach(square => {
@@ -30,8 +31,7 @@ squares.forEach(square => {
 })
 
 function moveMole () {
-    let timerId = null
-    timerId = setInterval(randomSquare, 1000)
+    timerId = setInterval(randomSquare, 500)
 }
 
 moveMole()
@@ -43,6 +43,7 @@ function countDown() {
     if (currentTime == 0) {
         clearInterval(countDownTimerId)
         alert("Game over! Your final score is" + result)
+        clearInterval(timerId)
     }
 }
 
